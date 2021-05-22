71b86a361d335d06341ba08b74e8840ef47c5f2f
pramilaniroshan <pramilaniroshan@gmail.com>
2021-05-22 20:03:14 +0530

    user menu logic added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -70,22 +70,45 @@ class Header extends Component {
      </li>
      )}
      
+     {currentUser && (
 
+<li class="nav-item dropdown connection"> <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="fas fa-fw fa-th"></i> </a>
+<ul class="dropdown-menu dropdown-menu-right connection-dropdown">
+    <li class="connection-list">
+        <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 "> <a href="#" class="connection-item"><img src="https://img.icons8.com/nolan/100/000000/github.png" alt="" /> <span>Github</span></a> </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 "> <a href="#" class="connection-item"><img src="https://img.icons8.com/color/100/000000/dribbble.png" alt="" /> <span>Dribbble</span></a> </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 "> <a href="#" class="connection-item"><img src="https://img.icons8.com/color/100/000000/dropbox.png" alt="" /> <span>Dropbox</span></a> </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 "> <a href="#" class="connection-item"><img src="https://img.icons8.com/color/100/000000/bitbucket.png" alt="" /> <span>Bitbucket</span></a> </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 "> <a href="#" class="connection-item"><img src="https://img.icons8.com/color/100/000000/remove-ads.png" alt="" /><span>Facebook ads</span></a> </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 "> <a href="#" class="connection-item"><img src="https://img.icons8.com/wired/64/000000/slack.png" alt="" /> <span>Slack</span></a> </div>
+        </div>
+    </li>
+    <li>
+        <div class="conntection-footer"><a href="#">More</a></div>
+    </li>
+</ul>
+</li>
+
+
+     )}
+     
      {currentUser ?
-     (<li class="nav-item dropdown">
-       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         Profile
-       </a>
-       
-       <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-       <Link to={"/profile"} className="dropdown-item">My Profile</Link>
-       <Link to={"/profile"} className="dropdown-item">Create a Service</Link>
-       <Link to={"/profile"} className="dropdown-item">Settings</Link>
-         <div class="dropdown-divider"></div>
-         <Link to={"/"} className="dropdown-item" onClick={this.logOut}>Log Out</Link>
-         
-       </div>
-     </li> ) 
+     ( 
+      <li class="nav-item dropdown nav-user"> <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="https://img.icons8.com/dusk/100/000000/user-female-circle.png" alt="" class="user-avatar-md rounded-circle" /></a>
+      <div class="dropdown-menu dropdown-menu-right nav-user-dropdown" aria-labelledby="navbarDropdownMenuLink2">
+          <div class="nav-user-info">
+              <h5 class="mb-0 text-white nav-user-name">{this.state.currentUser.username}</h5> <span class="status"></span><span class="ml-2">Available</span>
+          </div> 
+          <Link to={"/profile"} class="dropdown-item" ><i class="fas fa-user mr-2"></i>Account</Link>
+          <Link to={"/profile"} class="dropdown-item" ><i class="fas fa-cog mr-2"></i>Setting</Link>
+          <Link to={"/"} class="dropdown-item" onClick={this.logOut} ><i class="fas fa-power-off mr-2"></i>Logout</Link>
+          
+      </div>
+  </li>
+     ) 
      :
      (
        <li class="nav-item">
@@ -98,32 +121,8 @@ class Header extends Component {
                 
                 
                 
-                <li class="nav-item dropdown connection"> <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="fas fa-fw fa-th"></i> </a>
-                    <ul class="dropdown-menu dropdown-menu-right connection-dropdown">
-                        <li class="connection-list">
-                            <div class="row">
-                                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 "> <a href="#" class="connection-item"><img src="https://img.icons8.com/nolan/100/000000/github.png" alt="" /> <span>Github</span></a> </div>
-                                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 "> <a href="#" class="connection-item"><img src="https://img.icons8.com/color/100/000000/dribbble.png" alt="" /> <span>Dribbble</span></a> </div>
-                                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 "> <a href="#" class="connection-item"><img src="https://img.icons8.com/color/100/000000/dropbox.png" alt="" /> <span>Dropbox</span></a> </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 "> <a href="#" class="connection-item"><img src="https://img.icons8.com/color/100/000000/bitbucket.png" alt="" /> <span>Bitbucket</span></a> </div>
-                                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 "> <a href="#" class="connection-item"><img src="https://img.icons8.com/color/100/000000/remove-ads.png" alt="" /><span>Facebook ads</span></a> </div>
-                                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 "> <a href="#" class="connection-item"><img src="https://img.icons8.com/wired/64/000000/slack.png" alt="" /> <span>Slack</span></a> </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="conntection-footer"><a href="#">More</a></div>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown nav-user"> <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="https://img.icons8.com/dusk/100/000000/user-female-circle.png" alt="" class="user-avatar-md rounded-circle" /></a>
-                    <div class="dropdown-menu dropdown-menu-right nav-user-dropdown" aria-labelledby="navbarDropdownMenuLink2">
-                        <div class="nav-user-info">
-                            <h5 class="mb-0 text-white nav-user-name">Tikoha Samga</h5> <span class="status"></span><span class="ml-2">Available</span>
-                        </div> <a class="dropdown-item" href="#"><i class="fas fa-user mr-2"></i>Account</a> <a class="dropdown-item" href="#"><i class="fas fa-cog mr-2"></i>Setting</a> <a class="dropdown-item" href="#"><i class="fas fa-power-off mr-2"></i>Logout</a>
-                    </div>
-                </li>
+                
+                
             </ul>
         </div>
     </nav>
