51905d9171bef4be5fd3d3d9c9522a3ec1c3d9ff
pramilaniroshan <pramilaniroshan@gmail.com>
2021-07-16 12:12:15 +0530

    header coditional rendering done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -42,7 +42,7 @@ class Header extends Component {
         <>
 
 <div class="dashboard-header">
-    <nav class="navbar navbar-expand-lg bg-white fixed-top collapse navbar-collapse" > <a class="navbar-brand" href="#">Smart Home</a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+    <nav class="navbar navbar-expand-lg bg-white fixed-top collapse navbar-collapse" > <a class="navbar-brand" href="/">Smart Home</a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
         <div class="collapse navbar-collapse " id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto navbar-right-top">
             
