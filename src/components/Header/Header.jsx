250ca009648087c505cc4ce52c0062d85a5aaa39
pramilaniroshan <pramilaniroshan@gmail.com>
2021-07-27 14:11:42 +0530

    react link added to navbar /

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -55,7 +55,7 @@ class Header extends Component {
         <>
 
 <div class="dashboard-header">
-    <nav class="navbar navbar-expand-lg bg-white fixed-top collapse navbar-collapse" > <a class="navbar-brand" href="/">Smart Home</a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+    <nav class="navbar navbar-expand-lg bg-white fixed-top collapse navbar-collapse" > <Link class="navbar-brand" to="/">Smart Home.lk</Link> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
         <div class="collapse navbar-collapse " id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto navbar-right-top">
             
