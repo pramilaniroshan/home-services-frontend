6cbc63be186f80911a855c46bea53997127aa119
pramilaniroshan <pramilaniroshan@gmail.com>
2021-07-27 14:10:26 +0530

    service card style changed

--- src/components/Servicecard/index.jsx
+++ src/components/Servicecard/index.jsx
@@ -1,59 +1,49 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './style.css'
+import authService from '../../Services/auth.service';
+import md5 from 'md5';
 import avatar from '../../assets/avatar.png'
 
 export default function index(props) {
+
+    var user = authService.getCurrentUser();
+    if(user){
+    var avatar = `https://www.gravatar.com/avatar/${md5(user.email)}?s=500`;
+    }
     const service = props.service;
+ let link =`/show-service/${service._id}`
+ console.log(service)
     return (
         
-            <div class="col-lg-3 ">
-                <div class="text-center card-box ">
-                    <div class="member-card pt-2 pb-2">
-                        <div class="thumb-lg member-thumb mx-auto">
-                        
-                        <img src={avatar} class="rounded-circle img-thumbnail" alt="profile-image" /></div>
-                        <div class="">
-                            <h4>{service.title}</h4>
-                            <p class="text-muted">@Founder <span>| </span><span><a href="#" class="text-pink">websitename.com</a></span></p>
-                        </div>
-                        <ul class="social-links list-inline">
-                       
-                            <li class="list-inline-item"><a title="" data-placement="top" data-toggle="tooltip" class="tooltips" href="" data-original-title="Facebook"> <i class="fab fa-facebook fa-lg "></i></a></li>
-                            <li class="list-inline-item"><a title="" data-placement="top" data-toggle="tooltip" class="tooltips" href="" data-original-title="Facebook"> <i class="fab fa-whatsapp fa-lg "></i></a></li>
-                            <li class="list-inline-item"><a title="" data-placement="top" data-toggle="tooltip" class="tooltips" href="" data-original-title="Facebook"> <i class="fab fa-skype fa-lg "></i></a></li>
-                            
-                        </ul>
-                        <button type="button" class="btn btn-primary mt-3 btn-rounded waves-effect w-md waves-light profile-shine">Message Now</button>
-                        <div class="mt-4">
-                            <div class="row">
-                                <div class="col-4">
-                                    <div class="mt-3">
-                                        <h4>2563</h4>
-                                        <p class="mb-0 text-muted">Wallets Balance</p>
-                                    </div>
-                                </div>
-                                <div class="col-4">
-                                    <div class="mt-3">
-                                        <h4>6952</h4>
-                                        <p class="mb-0 text-muted">Income amounts</p>
-                                    </div>
-                                </div>
-                                <div class="col-4">
-                                    <div class="mt-3">
-                                        <h4>1125</h4>
-                                        <p class="mb-0 text-muted">Total Transactions</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+    
+  
+  
+        <div class="col-md-4 col-lg-4 card3">
+            <div class="card shadow-sm border-light mb-4">
+                <a href="#" class="position-relative">
+                    <img src={service.image} class="card-img-top" alt="image" /> </a>
+                <div class="card-body">
+                    <a href="#">
+                        <h5 class="font-weight-normal"><Link to={link}> {service.title}</Link></h5>
+                    </a>
+                    <div class="post-meta"><span class="small lh-120"><i class="fas fa-map-marker-alt mr-2"></i>{props.place}</span></div>
+                    <div class="d-flex my-4">
+                        <i class="star fas fa-star text-warning"></i>
+                        <i class="star fas fa-star text-warning"></i>
+                        <i class="star fas fa-star text-warning"></i>
+                        <i class="star fas fa-star text-warning"></i>
+                        <i class="star fas fa-star text-warning"></i> <span class="badge badge-pill badge-secondary ml-2">5.0</span></div>
+                    <div class="d-flex justify-content-between">
+                        <div class="col pl-0"><span class="text-muted font-small d-block mb-2">Monthly</span> <span class="h5 text-dark font-weight-bold">3500$</span></div>
+                        <div class="col"><span class="text-muted font-small d-block mb-2">Seats</span> <span class="h5 text-dark font-weight-bold">10</span></div>
+                        <div class="col pr-0"><span class="text-muted font-small d-block mb-2">Sq.Ft</span> <span class="h5 text-dark font-weight-bold">1500</span></div>
                     </div>
                 </div>
-            
-
-
-            
-
+            </div>
         </div>
+        
+        
         
     )
 }
