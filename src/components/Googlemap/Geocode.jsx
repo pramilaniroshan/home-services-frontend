73b148d26e7597af3d55add7e2bed07a41375ce3
pramilaniroshan <pramilaniroshan@gmail.com>
2021-07-27 14:13:26 +0530

    google geo code api connected

---
+++ src/components/Googlemap/Geocode.jsx
@@ -0,0 +1,45 @@
+
+import { GoogleComponent } from 'react-google-location' 
+
+//... 
+import React, { Component } from 'react';
+
+
+
+
+const API_KEY = ""  // how to get key - step are below
+
+class Geocode extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      place: null,
+    };
+  }
+
+  render() {
+    return (
+      <div >
+         <GoogleComponent
+         
+          apiKey={API_KEY}
+          language={'en'}
+          country={'country:in|country:us'}
+          coordinates={true}
+          currentCoordinates={{
+            "lat": 41.7151377,
+            "lng": 44.827096
+          }}
+          placeholder={'Start typing location'}
+          locationBoxStyle={'custom-style'}
+          locationListStyle={'custom-style-list'}
+          onChange={(e) => { this.setState({ place: e }) }} />
+      </div>
+
+    )
+  } 
+}
+
+
+export default Geocode;
+
