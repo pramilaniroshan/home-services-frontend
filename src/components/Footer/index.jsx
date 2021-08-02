85b01cc04b263cc96b9c60b4cf341a73fd478097
pramilaniroshan <pramilaniroshan@gmail.com>
2021-08-02 22:25:04 +0530

    footer content updated

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,47 +1,60 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './style.css'
 
 export default function index() {
     return (
-        <div>
+        
+        <div class="footer-section">
+            <div class="cta-sektion cta-padding35">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9 col-sm-9 col-xs-12">
+                <h3><span class="glyphicon glyphicon-cog "></span> <b>Become a Service Provider?</b> Post Service like a pro.</h3>
+            </div>
+            <div class="col-md-3 col-sm-3 col-xs-12 Tpadding10">
+               <Link to="/register"> <button type="button" class="btn btn-primary site-btn">Start Right Now</button></Link>
+            </div>
+        </div>
+    </div>
+</div>
+<div>
             <div class="container-fluid mx-auto">
     <div class="row justify-content-center top-part">
         
     </div>
     <div class="row">
-        <div class="line mb-3 mx-auto"></div>
+        <div class=" mb-3 mx-auto"></div>
         <div class="d-md-flex px-5 justify-content-around bd-highlight col-md-12 pt-5 pb-5 mb-3">
             <div class="p-2 flex-fill bd-highlight mb-5 mb-md-0">
-                <h3>itCraft</h3> <small>Copyright &copy 2019</small>
+                <h3>SMART HOME</h3> <small>Copyright © 2021</small>
             </div>
             <div class="p-2 flex-fill bd-highlight mb-3 mb-md-0">
-                <h5 class="places">BYDGOCZCZ</h5>
-                <p class="mb-0">Bydgoszcz Centrum Burnesa</p>
-                <p class="mb-0">ul Pzzenmdcwa 25, ibsura 24,</p>
-                <p class="mb-0">B5-7GB Bydgosrdz, Poland</p>
+                <h5 class="places">Colombo</h5>
+                <p class="mb-0">Street:  288 Galle Road, 04</p>
+                <p class="mb-0">City:   Colombo</p>
+                <p class="mb-0">State/province/area: Colombo</p>
+                <p class="mb-0">Phone number  (+94) ( 011) 2591918</p>
+                
             </div>
             <div class="p-2 flex-fill bd-highlight mb-3 mb-md-0">
-                <h5 class="places">TORUN</h5>
-                <p class="mb-0">ul Reyana 2-4</p>
-                <p class="mb-0">A0-100 Torun,</p>
-                <p class="mb-0">Poland</p>
+                <h5 class="places">Kandy</h5>
+                <p class="mb-0">Street:  288 Galle Road, 04</p>
+                <p class="mb-0">City:   Colombo</p>
+                <p class="mb-0">State/province/area: Colombo</p>
+                <p class="mb-0">Phone number  (+94) ( 011) 2591918</p>
+                
             </div>
             <div class="p-2 flex-fill bd-highlight mb-3 mb-md-0">
-                <h5 class="places">WARSAW</h5>
-                <p class="mb-0">Brain Embassy</p>
-                <p class="mb-0">Alaya Jerasublisuble RS123</p>
-                <p class="mb-0">02-222 Warsawa</p>
+                <h5 class="places">Rathnapura</h5>
+                <p class="mb-0">Street:  288 Galle Road, 04</p>
+                <p class="mb-0">City:   Colombo</p>
+                <p class="mb-0">State/province/area: Colombo</p>
+                <p class="mb-0">Phone number  (+94) ( 011) 2591918</p>
+                
             </div>
-            <div class="p-2 flex-fill bd-highlight mb-3 mb-md-0">
-                <h5 class="places">LONDON</h5>
-                <p class="mb-0">Google Campus</p>
-                <p class="mb-0">4-5 Barnhill, Shaneditch</p>
-                <p class="mb-0">London EC2A 4BK</p>
-            </div>
-            <div class="p-2 flex-fill bd-highlight mb-3 mb-md-0">
-                <h5 class="places">NORWAY</h5>
-                <p class="mb-0">Kristiansand</p>
-            </div>
+           
+            
         </div>
         <div class="line mb-3 mx-auto"></div>
     </div>
@@ -63,16 +76,17 @@ export default function index() {
                     <div class="p-2 flex-fill d-flex bd-highlight">Blog</div>
                     <div class="p-2 flex-fill d-flex bd-highlight">Carrer</div>
                     <div class="p-2 flex-fill d-flex bd-highlight">How we work</div>
-                    <div class="p-2 flex-fill d-flex bd-highlight">Manifesto</div>
+                    
                 </div>
             </div>
             <div class="p-2 align-self-center flex-fill bd-highlight">
-                <div class="fa fa-mobile px-2 grey-text">&nbsp;&nbsp;<span id="contact">888-777-666</span></div>
-                <div class="fa fa-envelope-o px-2 grey-text">&nbsp;&nbsp;info@itcraft.in</div>
+                <div class="fa fa-mobile px-2 grey-text">&nbsp;&nbsp;<span id="contact">0763190416</span></div>
+                <div class="fa fa-envelope-o px-2 grey-text">&nbsp;&nbsp;</div>
             </div>
         </div>
     </div>
 </div>
         </div>
+</div>  
     )
 }
